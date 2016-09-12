a94f362fb8096fc5579a40542806f32144bcbbb7
Zhang Tianwei <zhang@ynl.t.u-tokyo.ac.jp>
2016-09-12 18:31:11 +0900

    add ndt form pcl

---
+++ build/moving_det/CMakeFiles/normal_NDT.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/normal_NDT.dir/src/normal_NDT.cpp.o"
+  "/home/mech-user/motion_det/devel/lib/moving_det/normal_NDT.pdb"
+  "/home/mech-user/motion_det/devel/lib/moving_det/normal_NDT"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/normal_NDT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
